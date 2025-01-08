672e456a20a51f2eac729e7c29e5621423a2e4a6
KingCodexSal <kingsalvy983@gmail.com>
2025-01-08 12:37:15 +0100

    10

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar: React.FC<SidebarProps> = ({
     <div
       className={`fixed inset-y-0 left-0 transform ${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-      } transition-transform duration-300 ease-in-out bg-gray-800 text-white w-64`}
+      } md:translate-x-0 transition-transform duration-300 ease-in-out bg-gray-800 text-white w-64`}
     >
       <div className="flex items-center justify-center h-16 shadow-md">
         <h1 className="text-2xl font-semibold">Dashboard</h1>
