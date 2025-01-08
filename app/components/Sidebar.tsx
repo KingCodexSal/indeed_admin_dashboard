eba37ab9903979edf10c832e0526f4a72c46bd04
KingCodexSal <kingsalvy983@gmail.com>
2025-01-08 12:33:42 +0100

    9

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,34 +1,45 @@
-"use client";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers, faBriefcase } from "@fortawesome/free-solid-svg-icons";
 
-const Sidebar = ({ activeTab, setActiveTab, isSidebarOpen }) => {
+interface SidebarProps {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+  isSidebarOpen: boolean;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+  activeTab,
+  setActiveTab,
+  isSidebarOpen,
+}) => {
   return (
     <div
       className={`fixed inset-y-0 left-0 transform ${
-        isSidebarOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
-      } transition-transform duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-gray-700 text-white w-64 z-50 shadow-sm shadow-[#333]`}
+        isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 ease-in-out bg-gray-800 text-white w-64`}
     >
-      <div className="mt-2 p-4 text-2xl font-bold">Admin Dashboard</div>
-      <nav className="flex-1 p-4">
+      <div className="flex items-center justify-center h-16 shadow-md">
+        <h1 className="text-2xl font-semibold">Dashboard</h1>
+      </div>
+      <nav className="mt-10">
         <button
-          className={`w-full text-left p-4 mb-2 rounded flex items-center space-x-2 ${
-            activeTab === "users" ? "bg-gray-700" : "hover:bg-gray-800"
-          }`}
           onClick={() => setActiveTab("users")}
+          className={`flex items-center px-4 py-2 mt-2 text-sm font-medium rounded-lg ${
+            activeTab === "users" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}
         >
-          <FontAwesomeIcon icon={faUsers} />
-          <span>Users</span>
+          <FontAwesomeIcon icon={faUsers} className="mr-3" />
+          Users
         </button>
         <button
-          className={`w-full text-left p-4 mb-2 rounded flex items-center space-x-2 ${
-            activeTab === "jobs" ? "bg-gray-700" : "hover:bg-gray-800"
-          }`}
           onClick={() => setActiveTab("jobs")}
+          className={`flex items-center px-4 py-2 mt-2 text-sm font-medium rounded-lg ${
+            activeTab === "jobs" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}
         >
-          <FontAwesomeIcon icon={faBriefcase} />
-          <span>Jobs</span>
+          <FontAwesomeIcon icon={faBriefcase} className="mr-3" />
+          Jobs
         </button>
       </nav>
     </div>
